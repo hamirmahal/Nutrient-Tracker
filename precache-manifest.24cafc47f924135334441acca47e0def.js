b091adefa6ed545cbba5bba14e900997ea36ac9b
Hamir Mahal <hamirmahal@gmail.com>
2020-07-22 23:06:39 -0700

    Updates

---
+++ precache-manifest.24cafc47f924135334441acca47e0def.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9389f1abb1a01635393fde5fe0422963",
+    "url": "/Nutrient-Tracker/index.html"
+  },
+  {
+    "revision": "52f18059f3013849f3af",
+    "url": "/Nutrient-Tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b33bca9eafaa376f0ec6",
+    "url": "/Nutrient-Tracker/static/js/2.9b657bc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nutrient-Tracker/static/js/2.9b657bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f18059f3013849f3af",
+    "url": "/Nutrient-Tracker/static/js/main.ad24e2d4.chunk.js"
+  },
+  {
+    "revision": "e4bd4d52a7b3afcfad21",
+    "url": "/Nutrient-Tracker/static/js/runtime-main.1762f36b.js"
+  },
+  {
+    "revision": "556bbdadeaa627f95d32205bab1eb2b2",
+    "url": "/Nutrient-Tracker/static/media/AlmondsHamirMahal.556bbdad.png"
+  }
+]);
