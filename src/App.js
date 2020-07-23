82e76dd2efd866d7ed0e041c8c98bc5d4cdb0a18
Hamir Mahal <hamirmahal@gmail.com>
2020-07-22 23:02:02 -0700

    Changed default title, favicons, and logos!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import almonds from './AlmondsHamirMahal.png';
 import './App.css';
 
 function App() {
+  const dailyTotal = [];
+  let url = 'https://api.nal.usda.gov/fdc/v1/food';
+  url += '/169991?api_key=DEMO_KEY';
+  fetch(url).then(r => r.json()).then(c => {
+    console.log(c);
+    console.log(c.foodNutrients);
+    c.foodNutrients.forEach(foodNutrient => {
+      dailyTotal.push({
+        [foodNutrient.nutrient.name]: foodNutrient.amount
+      });
+    });
+    console.log(dailyTotal);
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={almonds} className="App-logo"
+          alt="Almonds from Hamir" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your daily totals are below.
         </p>
         <a
           className="App-link"
